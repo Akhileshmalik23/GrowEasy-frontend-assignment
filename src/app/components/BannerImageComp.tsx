683cc9235ebbdd59c180d91ff0c186ede531e614
Akhileshmalik23 <akhileshmalik908@gmail.com>
2024-08-05 22:36:48 +0530

    final commit

--- src/app/components/BannerImageComp.tsx
+++ src/app/components/BannerImageComp.tsx
@@ -32,7 +32,7 @@ const BannerImageComp: React.FC<BannerProps> = ({ id, title, description, button
         <h2 className='text-4xl   font-bold mt-6 ml-8' style={{ color }}>{title}</h2>
         <p className="mt-3 ml-5 text-gray-700">{description}</p>
 
-        <button className="mt-40 ml-6 w-40 bg-blue-500 text-white py-2 px-4 rounded-lg shadow-lg hover:bg-black hover:border-red-400 hover:border-2 hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105">
+        <button className="mt-40 ml-6 w-40 bg-gray-600 text-white py-2 px-4 rounded-lg shadow-lg hover:bg-slate-900 hover:border-red-400 hover:border-spacing-1 hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105">
           {button}
         </button>
 
